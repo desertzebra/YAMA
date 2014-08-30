d2c34ffc149d40ac1250d74852e006a533e3b9a1
desertzebra <desertzebra@desertzebra-VirtualBox.(none)>
2014-08-30 17:56:17 +0200

    updated states

--- client/yamaapi/model/Model.php
+++ client/yamaapi/model/Model.php
@@ -1,37 +1,59 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author desertzebra
  */
 class Model {
+ /*
+  * State of this model. The state can be one of:
+  * STATE_SAVE; ready to save
+  * STATE_DELETE;marked as delete
+  * STATE_UPDATE;needs updating
+  * STATE_LOAD; stable state, with nothing to do.
+  */
  protected $state;
- protected $lastUpdateTime;
  const STATE_DELETE = 2;
  const STATE_SAVE = 1;
  const STATE_UPDATE = 3;
- const STATE_LOAD = 0;
- 
+ const STATE_STABLE = 0;
+ const STATE_LOAD = 4;
+ /*
+  * Timestamp, since last state.
+  */
+ protected $lastUpdateTIme; 
+
+ /*
+  * Constructor, which sets the initial condition as load, so the model can be
+  * loaded the first time.
+  */
  public function __construct(){
      $this->state = self::STATE_LOAD;
      $this->lastUpdateTime = 0;
-}
+ }
+ /*
+  * Parent load method to update the lastUpdateTime and set the state to stable.
+  * This should be called at end of the extended load method.
+  */
  protected function load(){
      $lastUpdateTime = time();
-     $state = "updated";
+     $state = self::STATE_STABLE;
 //     echo $state + " " + $lastUpdateTime;
  }
+ /*
+  * Parent save method to update the lastUpdateTime and set the state to stable.
+  * This should be called at end of the extended save method.
+  */
 protected function save(){
      $lastUpdateTime = time();
-     $state = "save";
+     $state = self::STATE_STABLE;
 //     echo $state + " " + $lastUpdateTime;
 }
+
+ /*
+  * Check if the lastUpdateTime is not 0 or greater than the updateInterval
+  * defined in the config file.
+  */
 protected function requiresLoad(){
     if(!isset($this->lastUpdateTime) || 
             $this->lastUpdateTime==0 || 
@@ -44,6 +66,9 @@ protected function requiresLoad(){
         return false;
     }
 }
+ /*
+  * Check if the model requires saving.
+  */ 
 protected function requiresSave(){
     if($this->state === self::STATE_SAVE){
         return true;
@@ -52,6 +77,9 @@ protected function requiresSave(){
         return false;
     }
 }
+ /*
+  * Check if the model requires deletion.
+  */
 protected function requiresDelete(){
     if($this->state === self::STATE_DELETE){
         return true;
@@ -68,9 +96,15 @@ protected function requiresUPDATE(){
         return false;
     }
 }
+ /*
+  * Utility function to get all variables of this object.
+  */
 public function getAllVars(){
        return get_object_vars($this);
     }
+ /*
+  * Utility function to load a php class dynamically.
+  */
 public static function loadclass($classname,$filename=""){
   if(class_exists($classname)){
     return true;
